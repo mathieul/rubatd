b7a5b09fe14cc25286a935d2103b8ee9626f56b6
Mathieu Lajugie <mathieul@gmail.com>
2013-03-10 21:49:43 -0700

    fix data store spec label

--- spec/rubatd/data_store_spec.rb
+++ spec/rubatd/data_store_spec.rb
@@ -17,7 +17,7 @@ describe DataStore do
     expect(store.db).to be_a(Redis)
   end
 
-  it "#save generates a model id and saves the model" do
+  it "#create generates a model id and saves the model" do
     Rubatd::Stores::RedisDSModel.any_instance.should_receive(:generate_model_id)
     Rubatd::Stores::RedisDSModel.any_instance.should_receive(:save)
     store.create(DSModel.new)
