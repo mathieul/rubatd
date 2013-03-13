5e94710c3801efd183e95e04f3e36976074c96e9
Mathieu Lajugie <mathieul@gmail.com>
2013-03-12 21:42:12 -0700

    adapt provisioning spec to read a model from redis

--- spec/acceptance/provisioning_spec.rb
+++ spec/acceptance/provisioning_spec.rb
@@ -7,7 +7,8 @@ feature "Provisioning objects" do
     store = DataStore.new(:redis, Redis, redis_config)
     the_advisors = Team.new(name: "Advisors")
     store.save(the_advisors)
-    mathieu = Teammate.new(name: "mathieu", team: the_advisors)
-    store.save(mathieu)
+    teammate = Teammate.new(name: "mathieu", team: the_advisors)
+    store.save(teammate)
+    mathieu = store["Teammate"][teammate.id]
   end
 end
