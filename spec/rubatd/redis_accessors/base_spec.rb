dd1d1d7281af68dcb2d23dbd5a86550fe286cb21
Mathieu Lajugie <mathieul@gmail.com>
2013-03-16 10:03:34 -0700

    refactor redis accessor base spec

--- spec/rubatd/redis_accessors/base_spec.rb
+++ spec/rubatd/redis_accessors/base_spec.rb
@@ -15,36 +15,37 @@ class Rubatd::RedisAccessors::Actor < RedisAccessors::Base
 end
 
 describe RedisAccessors::Base do
-  let(:accessor) { RedisAccessors::Movie.new(Redis.new(redis_config)) }
+  let(:redis) { Redis.new(redis_config) }
+  let(:movie_accessor) { RedisAccessors::Movie.new(redis) }
 
   context "#save: save to redis" do
     let(:model) { Movie.new("title" => "Goldfinger", "number" => "007") }
 
     it "#save raises an error if model is not valid" do
       model.should_receive(:valid?).and_return(false)
-      expect { accessor.save(model) }.to raise_error(ModelInvalid)
+      expect { movie_accessor.save(model) }.to raise_error(ModelInvalid)
     end
 
     it "sets the model id if not persisted" do
-      accessor.save(model)
+      movie_accessor.save(model)
       expect(model.id).to eq("1")
     end
 
     it "doesn't set the model id if already set" do
       model.id = "42"
-      accessor.save(model)
+      movie_accessor.save(model)
       expect(model.id).to eq("42")
     end
 
     it "adds the model id to the list of all model ids" do
       2.times do
-        accessor.save(model)
+        movie_accessor.save(model)
         expect(redis.smembers("Movie:all")).to eq(["1"])
       end
     end
 
     it "stores the model attributes" do
-      accessor.save(model)
+      movie_accessor.save(model)
       expect(redis.hgetall("Movie:1")).to eq(
         "title" => "Goldfinger", "number" => "007"
       )
@@ -52,25 +53,24 @@ describe RedisAccessors::Base do
 
     it "sets the model as persisted" do
       model.should_receive(:persisted!)
-      accessor.save(model)
+      movie_accessor.save(model)
     end
   end
 
   context "#get: read model from redis by id" do
     it "finds the model id" do
-      accessor.save(Movie.new("title" => "Goldfinger", "number" => "007"))
-      model = accessor.get("1")
+      movie_accessor.save(Movie.new("title" => "Goldfinger", "number" => "007"))
+      model = movie_accessor.get("1")
       expect(model.attributes).to eq("title" => "Goldfinger", "number" => "007")
     end
 
     it "raises an error if no model exists for this id" do
-      expect { accessor.get("does-not-exist") }.to raise_error(ModelNotFound)
+      expect { movie_accessor.get("does-not-exist") }.to raise_error(ModelNotFound)
     end
   end
 
   context "model references" do
-    let(:movie_accessor) { accessor }
-    let(:actor_accessor) { RedisAccessors::Actor.new(Redis.new(redis_config)) }
+    let(:actor_accessor) { RedisAccessors::Actor.new(redis) }
     let(:moonraker) { Movie.new("title" => "Moonraker").tap { |m| m.id = "007" } }
     let(:moore) do
       Actor.new("name" => "Roger Moore").tap { |moore| moore.id = "3" }
