37e165b782edfcbc94aed5044a89a6f5cca22c6c
Mathieu Lajugie <mathieul@gmail.com>
2013-03-16 10:03:51 -0700

    add message trap util class

--- lib/rubatd.rb
+++ lib/rubatd.rb
@@ -2,6 +2,7 @@ Rubatd = Module.new
 Rubatd::Error = Class.new(StandardError)
 
 require "rubatd/core_extensions"
+require "rubatd/utils/message_trap"
 require "rubatd/models"
 require "rubatd/redis_accessors"
 require "rubatd/data_store"
