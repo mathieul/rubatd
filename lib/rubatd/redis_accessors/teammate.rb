53fbe2543999ec392e5350af4574b9f8ebe90e79
Mathieu Lajugie <mathieul@gmail.com>
2013-03-18 22:23:19 -0700

    remove unnecessary code

--- lib/rubatd/redis_accessors/teammate.rb
+++ lib/rubatd/redis_accessors/teammate.rb
@@ -2,11 +2,6 @@ module Rubatd
   module RedisAccessors
     class Teammate < Base
       reference "Team"
-
-      def attributes(model)
-        attributes = model.attributes.except("team")
-        attributes.merge("team_id" => model.team && model.team.id)
-      end
     end
   end
 end
