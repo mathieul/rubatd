9129be55484f40d4e30bd3a9581ccdeafe899a98
Mathieu Lajugie <mathieu@caring.com>
2013-03-20 22:57:38 -0700

    change how we exclude instance variables from model attributes

--- lib/rubatd/models/model.rb
+++ lib/rubatd/models/model.rb
@@ -4,6 +4,29 @@ module Rubatd
   class Model
     include Scrivener::Validations
 
+    attr_accessor :id
+    attr_reader   :persisted_attributes
+
+    class << self
+      def reserved_attributes
+        @reserved_attributes ||= default_reserved_attributes
+      end
+
+      def reserve_attributes(*attributes)
+        names = attributes.map { |attribute| :"@#{attribute}" }
+        reserved_attributes.push(*names)
+      end
+
+      private
+
+      def default_reserved_attributes
+        reservable = ancestors[1..-1].find { |klass| klass.respond_to?(:reserved_attributes) }
+        reservable ? reservable.reserved_attributes.dup : []
+      end
+    end
+
+    reserve_attributes :id, :errors, :persisted_attributes
+
     def initialize(attributes = {})
       attributes.each do |name, value|
         send("#{name}=", value) if respond_to?(name)
@@ -15,16 +38,9 @@ module Rubatd
       # can be overriden by children
     end
 
-    def id
-      @_id
-    end
-
-    def id=(id)
-      @_id = id
-    end
-
     def attributes
-      names = instance_variables.reject { |name| name == :@errors || name[1] == "_" }
+      except = self.class.reserved_attributes
+      names = instance_variables.reject { |name| except.include?(name) }
       names.each_with_object({}) do |name, attributes|
         value = instance_variable_get(name)
         attributes[name[1..-1]] = value unless value.nil?
@@ -36,19 +52,15 @@ module Rubatd
     end
 
     def persisted?
-      !@_persisted_attributes.nil?
+      !@persisted_attributes.nil?
     end
 
     def persisted!(persisted_attributes)
-      @_persisted_attributes = persisted_attributes
+      @persisted_attributes = persisted_attributes
     end
 
     def not_persisted!
-      @_persisted_attributes = nil
-    end
-
-    def persisted_attributes
-      @_persisted_attributes
+      @persisted_attributes = nil
     end
   end
 end
