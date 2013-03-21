aa8bfab48fdb65d04ee6fd4de5155ca5d3df95e9
Mathieu Lajugie <mathieu@caring.com>
2013-03-20 23:41:24 -0700

    make queue accessor persist sorted set of tasks

--- lib/rubatd/redis_accessors/queue.rb
+++ lib/rubatd/redis_accessors/queue.rb
@@ -2,6 +2,12 @@ module Rubatd
   module RedisAccessors
     class Queue < Base
       reference "Team"
+      embedded_key :tasks do |model, key|
+        to_add = model.enqueued_tasks.map { |task| [task.created_ts, task.id] }.flatten
+        key.zadd(*to_add) unless to_add.empty?
+        to_del = model.dequeued_tasks.map(&:id)
+        key.zrem(*to_del) unless to_del.empty?
+      end
     end
   end
 end
--- spec/rubatd/redis_accessors/queue_spec.rb
+++ spec/rubatd/redis_accessors/queue_spec.rb
@@ -12,15 +12,38 @@ describe RedisAccessors::Queue do
     expect(accessor).to be_an(RedisAccessors::Base)
   end
 
-  it "#save raises an error if team is not persisted" do
-    queue.team = build(:team)
-    expect { accessor.save(queue) }.to raise_error(ModelInvalid)
+  context "#save" do
+    it "raises an error if team is not persisted" do
+      queue.team = build(:team)
+      expect { accessor.save(queue) }.to raise_error(ModelInvalid)
+    end
+
+    it "persists the team id" do
+      accessor.save(queue)
+      expect(redis.hgetall("Queue:1")["team_id"]).to eq("1")
+    end
+
+    it "adds the tasks enqueued to the sorted set of tasks" do
+      queue.enqueue(task = create(:task, id: "t123"))
+      accessor.save(queue)
+      expect(redis.zrange("Queue:1:tasks", 0, -1)).to eq(["t123"])
+    end
+
+    it "removes the tasks dequeued from the sorted set of tasks" do
+      queue.enqueue(task = create(:task, id: "t123"))
+      accessor.save(queue)
+      queue.dequeue(task)
+      accessor.save(queue)
+      expect(redis.zrange("Queue:1:tasks", 0, -1)).to eq([])
+    end
   end
 
-  it "#save persists the team id" do
-    accessor.save(queue)
-    expect(redis.hgetall("Queue:1")).to eq(
-      "name" => "Sales", "team_id" => "1"
-    )
+  context "#delete" do
+    it "removes the sorted set of tasks of the queue" do
+      queue.enqueue(task = create(:task, id: "t123"))
+      accessor.save(queue)
+      accessor.delete(queue)
+      expect(redis.zrange("Queue:1:tasks", 0, -1)).to eq([])
+    end
   end
 end
